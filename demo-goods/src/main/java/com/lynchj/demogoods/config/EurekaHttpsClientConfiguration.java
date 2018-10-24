4ec7a66bd1cef9d13d11d1d8de43e88c40805107
郭青松 <lynchj@163.com>
2018-10-24 14:48:23 +0800

    Spring Cloud Eureka HTTP Basic 认证

---
+++ demo-goods/src/main/java/com/lynchj/demogoods/config/EurekaHttpsClientConfiguration.java
@@ -0,0 +1,56 @@
+package com.lynchj.demogoods.config;
+
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl;
+import lombok.Data;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+/**
+ * @Author：大漠知秋
+ * @Description：Eureka Client 针对 Eureka Server 的 HTTPS 请求配置
+ * @CreateDate：4:13 PM 2018/10/23
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "eureka.client.ssl")
+public class EurekaHttpsClientConfiguration {
+
+    private String keyStore;
+
+    private String keyStorePassword;
+
+    @Bean
+    @ConditionalOnProperty(
+            value = "eureka.client.secure-port-enabled",
+            havingValue = "true"
+    )
+    public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException {
+        EurekaJerseyClientImpl.EurekaJerseyClientBuilder builder = new EurekaJerseyClientImpl.EurekaJerseyClientBuilder();
+        builder.withClientName("eureka-https-client");
+        SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(
+                        this.getClass().getClassLoader().getResource(this.keyStore), this.keyStorePassword.toCharArray()
+                )
+                .build();
+        builder.withCustomSSL(sslContext);
+
+        builder.withMaxTotalConnections(10);
+        builder.withMaxConnectionsPerHost(10);
+
+        DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
+        args.setEurekaJerseyClient(builder.build());
+        return args;
+    }
+
+}
---
+++ demo-order/src/main/java/com/lynchj/demoorder/controller/GitHubController.java
@@ -0,0 +1,35 @@
+package com.lynchj.demoorder.controller;
+
+import com.lynchj.demoorder.feign.GitHubFeign;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author：大漠知秋
+ * @Description：使用 Feign 访问 Github 查询 API
+ * @CreateDate：2:42 PM 2018/10/24
+ */
+@RestController
+@RequestMapping(
+        value = "/github",
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+)
+public class GitHubController {
+
+    @Resource
+    private GitHubFeign gitHubFeign;
+
+    @RequestMapping(
+            value = "/search/repositories",
+            method = RequestMethod.GET
+    )
+    String searchRepo(@RequestParam("q") String q) {
+        return gitHubFeign.searchRepo(q);
+    }
+
+}
