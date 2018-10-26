1ac4a70cbce5e4df1ad44654a7680c8d206dd44b
郭青松 <lynchj@163.com>
2018-10-26 10:48:59 +0800

    Spring Cloud Ribbon 配置文件方式设置单个服务的负载均衡策略

--- demo-order/src/main/java/com/lynchj/demoorder/DemoOrderApplication.java
+++ demo-order/src/main/java/com/lynchj/demoorder/DemoOrderApplication.java
@@ -1,16 +1,11 @@
 package com.lynchj.demoorder;
 
-import com.lynchj.demoorder.annotation.AvoidScan;
-import com.lynchj.demoorder.config.RibbonRandomLoadBalancingConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -23,16 +18,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 /** 开启 Feign 扫描支持 */
 @EnableFeignClients
-/** 配置针对单个服务的 Ribbon 负载均衡策略 **/
-@RibbonClient(
-        name = "demo-goods", configuration = RibbonRandomLoadBalancingConfiguration.class
-)
-/** 此处配置根据标识 @AvoidScan 过滤掉需要单独配置的 Ribbon 负载均衡策略，不然就会作用于全局，启动就会报错 */
-@ComponentScan(
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.ANNOTATION, value = AvoidScan.class
-        )
-)
 public class DemoOrderApplication {
 
     public static void main(String[] args) {
