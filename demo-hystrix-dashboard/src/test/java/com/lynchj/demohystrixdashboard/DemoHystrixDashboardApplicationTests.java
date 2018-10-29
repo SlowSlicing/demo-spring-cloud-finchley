228abf2954d898b91b4061567ee381fc2d3cebaf
郭青松 <lynchj@163.com>
2018-10-29 14:15:53 +0800

    Spring Cloud Hystrix Dashboard 简单使用

---
+++ demo-hystrix-dashboard/src/test/java/com/lynchj/demohystrixdashboard/DemoHystrixDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lynchj.demohystrixdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoHystrixDashboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
